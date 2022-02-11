5f88a49b763fb419782030bd037dc7803b8211c1
albercol <58440433+albercol@users.noreply.github.com>
2022-02-11 11:14:27 +0100

    Update main.cpp

--- Practica5/main.cpp
+++ Practica5/main.cpp
@@ -1,21 +1,16 @@
-//ALUMNOS:
-//GRUPO: B34  LAB: 1
-//COLLADO MAMBLONA, ALBERTO
-//G”MEZ ALONSO, JOS… LUIS
-
 #include "cola.h"
 #include <iostream>
 using namespace std;
 
 /**
-Este programa lee por la entrada est·ndar lÌneas con el siguiente formato:
+Este programa lee por la entrada est√°ndar l√≠neas con el siguiente formato:
 - Una secuencia de enteros (excluido el -1), que se interpreta como una
   cola.
 - El -1 (centinela que marca el final de la cola)
-- Una posiciÛn (entero >= 0)
-Entonces, ejecuta la operaciÛn 'llevarAlPrincipio' sobre la cola indicada, y escribe
-por pantalla el resultado (o POSICION_INVALIDA en caso de que la posiciÛn
-no sea v·lida)
+- Una posici√≥n (entero >= 0)
+Entonces, ejecuta la operaci√≥n 'llevarAlPrincipio' sobre la cola indicada, y escribe
+por pantalla el resultado (o POSICION_INVALIDA en caso de que la posici√≥n
+no sea v√°lida)
 
 Ejemplo de entrada:
 1 2 3 4 5 -1 1
